8e7afd63f2f7f401ee9d3636b26b09aee1aa25dd
alfred <brilychen@gmail.com>
2018-06-15 11:25:15 +0800

    complete 3-8

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,20 +1,45 @@
 package Test;
 
 import Array.Array;
+import Queue.Queue;
+import Queue.ArrayQueue;
+import Queue.LoopQueue;
 import Stack.ArrayStack;
 
+import java.util.Random;
+
 public class Main {
+
+    //测试使用q运行opCount个enQueue和deQueue操作所需要的时间，单位:秒
+    private static double testQueue(Queue<Integer> q, int opCount) {
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++)
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+
+        for (int i = 0; i < opCount; i++)
+            q.dequeue();
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
     public static void main(String[] args) {
 
-        ArrayStack<Integer> stack = new ArrayStack<Integer>();
+        int opCount = 100000;
 
-        for (int i = 0;i<5;i++){
-            stack.push(i);
-            System.out.println(stack);
-        }
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<Integer>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue, Time: " + time1 + " s");
 
-        stack.pop();
-        System.out.println(stack);
+        LoopQueue<Integer> loopQueue = new LoopQueue<Integer>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("ArrayQueue, Time: " + time2 + " s");
+
 
     }
 }
+
+
